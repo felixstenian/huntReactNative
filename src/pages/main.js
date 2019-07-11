7a6dd74a105a7e6f818365568234358573b91d8b
Stenia Felix <steniafelix@gmail.com>
2019-07-11 00:25:08 -0300

    Setting up the API query and setting in the header

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class main extends Component {
-  static: navigationOptions = {
+  static navigationOptions = {
     title: "JSHunt"
   };
 
--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,5 @@ export default createStackNavigator({
       backgroundColor: "#DA552F"
     },
     headerTintColor: "#FFF"
-  }
+  },
 });
